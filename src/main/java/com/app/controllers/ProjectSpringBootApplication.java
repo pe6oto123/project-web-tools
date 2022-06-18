c89a3f4b7816d49f160fa79b9b901b6d15b7cb9d
pe6oto123 <pe6oto123@gmail.com>
2022-06-18 20:02:45 +0300

    test automatic build

--- src/main/java/com/app/controllers/ProjectSpringBootApplication.java
+++ src/main/java/com/app/controllers/ProjectSpringBootApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProjectSpringBootApplication {
 	@GetMapping
 	public String message() {
-		return "message";
+		return "new message";
 	}
 
 	public static void main(String[] args) {
